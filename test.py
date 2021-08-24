f908329fcab9f4d6c5c965a434735458e0f6b3b3
Ha On Lim <limhaon0612@gmail.com>
2021-08-24 17:57:17 +0900

    엄청 간단한 숫자게임
    마지막 돌 가져가는 사람이 이김

--- test.py
+++ test.py
@@ -1,1 +1,23 @@
-print("hello world!!")
+a=800
+b=800
+while(1):
+    player1=input("player 1 입력하세요:")
+    if player1.split(",")[0]=="a":
+        a-=int(player1.split(",")[1])
+    elif player1.split(",")[0]=="b":
+        b-=int(player1.split(",")[1])
+    if a==0 and b==0: 
+        print("player 1 win")
+        break
+    print(a,b)
+    player2=input("player 2 입력하세요:")
+    if player2.split(",")[0]=="a":
+        a-=int(player2.split(",")[1])
+    
+    elif player2.split(",")[0]=="b":
+        b-=int(player2.split(",")[1])
+       
+    if a==0 and b==0: 
+        print("player 2 win")
+        break
+    print(a,b)    
